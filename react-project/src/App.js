356857fa36ee1fb6629833f4a081c817633d6543
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-05-26 16:00:22 -0400

    orderCoffee options

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,5 +1,49 @@
-function App() {
-  return <h1>Hi there</h1>;
+import React, { useState } from "react";
+const options = [
+  "Double Espresso",
+  "Mocha",
+  "Chocolate Syrup",
+  "Whipped Cream",
+];
+
+export default function OrderCoffee() {
+  const [selected, setSelected] = useState([]);
+
+  const toggleOptions = ({ target }) => {
+    const clickedOptions = target.value;
+
+    // function <setSelected> fills in the array [selected], which is displayed at the bottom in Order a [selected] coffee.
+    setSelected((prev) => {
+      if (prev.includes(clickedOptions)) {
+        // filter the clicked options out of state
+        return prev.filter((t) => t !== clickedOptions);
+      } else {
+        // add the clicked options to state
+        return [clickedOptions, ...prev];
+      }
+    });
+  };
+
+  return (
+    <div>
+      {options.map((option) => (
+        <button value={option} key={option} onClick={toggleOptions}>
+          {selected.includes(option) ? "Remove " : "Add "}
+          {option}
+        </button>
+      ))}
+      <p>Order a {selected.join(", ")} coffee.</p>
+    </div>
+  );
 }
 
-export default App;
+// NOTE - ways to work with setSelected
+// const handler = (event) => {
+//   const value = event.target.value;
+//   const newStateArray = [];
+//   setSelected(newStateArray);
+// // OR
+//   setSelected((prev)=>{
+//     return newStateArray
+//   });
+// };
